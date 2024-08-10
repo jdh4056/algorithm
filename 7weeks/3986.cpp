6c33771fd4294c16076010ee4199fa4f850d4ac6
jdh4056 <jdh4056@naver.com>
2024-08-10 14:40:18 +0900

    8.9

--- 7weeks/3986.cpp
+++ 7weeks/3986.cpp
@@ -4,10 +4,25 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-
     int count = 0;
-    for (int i = 0; i < N; ++i) {
-        string str;
-        cin >> str;
+
+    while (N--) {
+        string word;
+        cin >> word;
+        stack<char> s;
+
+        for (char c : word) {
+            if (!s.empty() && s.top() == c) {
+                s.pop();
+            } else {
+                s.push(c);
+            }
+        }
+
+        if (s.empty()) {
+            count++;
+        }
     }
+
+    cout << count;
 }
