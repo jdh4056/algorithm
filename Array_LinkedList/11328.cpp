b299a004bc2d7fe45689bb27817907071642913c
jdh4056 <jdh4056@naver.com>
2024-09-11 15:14:09 +0900

    9.10

--- Array_LinkedList/11328.cpp
+++ Array_LinkedList/11328.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 
 int main() {
+    int N;
+    cin >> N;
 
+    for (int i = 0; i < N; ++i) {
+        string a, b;
+        cin >> a >> b;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        unordered_set<string> S;
+        S.insert(a);
+
+        if (S.find(b) != S.end()) {
+            cout << "Possible" << '\n';
+        } else {
+            cout << "Impossible" << '\n';
+        }
+    }
 }
