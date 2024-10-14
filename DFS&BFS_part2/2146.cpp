b1baf1452eafd51d5086d31c0a4030ea5e74484c
jdh4056 <jdh4056@naver.com>
2024-10-14 19:36:53 +0900

    9.28 ~ 10.13

--- DFS&BFS_part2/2146.cpp
+++ DFS&BFS_part2/2146.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define X first
 #define Y second
 int board[102][102];
+int island[102][102];
 int dist[102][102];
-int dx[8] = {-2, 0, -1, 0, 1, 0, 2, 0};
-int dy[8] = {0, -2, 0, -1, 0, 1, 0, 2};
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
 int N;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     cin >> N;
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -17,25 +20,70 @@ int main() {
         }
     }
 
+    int id = 0;
+
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            int x,y;
-            cin >> x >> y;
-            dist[x][y] = 1;
-            queue<pair<int,int>> q;
-            q.push({x,y});
+            if (board[i][j] == 1 && island[i][j] == 0) {
+                id++;
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                island[i][j] = id;
 
-            while (!q.empty()) {
-                pair<int, int> cur = q.front();
-                q.pop();
-                for (int dir = 0; dir < 8; ++dir) {
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-                        continue;
-                    if ()
+                while (!q.empty()) {
+                    pair<int, int> cur = q.front();
+                    q.pop();
+                    for (int dir = 0; dir < 4; ++dir) {
+                        int nx = cur.X + dx[dir];
+                        int ny = cur.Y + dy[dir];
+                        if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+                            continue;
+                        if (board[nx][ny] == 1 && island[nx][ny] == 0) {  // 육지이면서 아직 방문 안 한 곳
+                            island[nx][ny] = id;
+                            q.push({nx, ny});
+                        }
+                    }
                 }
             }
         }
     }
+
+    int result = INT_MAX;
+
+
+    for (int i = 1; i <= id; ++i) {
+        queue<pair<int, int>> q;
+        memset(dist, -1, sizeof(dist));
+
+        for (int x = 0; x < N; ++x) {
+            for (int y = 0; y < N; ++y) {
+                if (island[x][y] == i) {
+                    q.push({x, y});
+                    dist[x][y] = 0;
+                }
+            }
+        }
+
+
+        while (!q.empty()) {
+            pair<int, int> cur = q.front();
+            q.pop();
+            for (int dir = 0; dir < 4; ++dir) {
+                int nx = cur.X + dx[dir];
+                int ny = cur.Y + dy[dir];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+                    continue;
+                if (dist[nx][ny] == -1) {
+                    dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+                    q.push({nx, ny});
+                }
+
+                if (island[nx][ny] != 0 && island[nx][ny] != i) {
+                    result = min(result, dist[cur.X][cur.Y]);
+                }
+            }
+        }
+    }
+
+    cout << result << '\n';
 }
--- DFS&BFS_part2/7562.cpp
+++ DFS&BFS_part2/7562.cpp
@@ -1,2 +1,38 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+#define X first
+#define Y second
+int dist[102][102];
+int dx[8] = {-2,-1,1,2,2,1,-1,-2};
+int dy[8] = {1,2,2,1,-1,-2,-2,-1};
+int T, N;
+
+int main() {
+    cin >> T;
+    cin >> N;
+
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            int x,y, goalX, goalY;
+            cin >> x >> y;
+            cin >> goalX >> goalY;
+            dist[x][y] = 1;
+            queue<pair<int,int>> q;
+            q.push({x,y});
+
+            while (dist) {
+                pair<int, int> cur = q.front();
+                q.pop();
+                for (int dir = 0; dir < 8; ++dir) {
+                    int nx = cur.X + dx[dir];
+                    int ny = cur.Y + dy[dir];
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+                        continue;
+
+                }
+            }
+        }
+    }
+}
