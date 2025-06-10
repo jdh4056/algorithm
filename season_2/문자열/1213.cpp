c3274e0dbd6df74a44cd94f45c50bbf194615e8a
jdh4056 <jdh4056@naver.com>
2025-06-10 21:32:30 +0900

    6.10

--- season_2/문자열/1213.cpp
+++ season_2/문자열/1213.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int cnt[200];
+int flag;
+char mid;
 int main() {
     string s; cin >> s;
 
+
+    string res;
+    for (char a : s) {
+        cnt[a]++;
+    }
+    for (int i = 'Z'; i >= 'A'; i--) {
+        if (cnt[i]) {
+            if (cnt[i] & 1) {
+                mid = char(i);
+                flag++;
+                cnt[i]--;
+            }
+            if (flag == 2) break;
+            for (int j = 0; j < cnt[i]; j += 2) {
+                res = char(i) + res;
+                res += char(i);
+            }
+        }
+    }
+
+    if (mid) {
+        res.insert(res.begin() + res.size() / 2, mid);
+    }
+    if (flag == 2) {
+        cout << "I'm Sorry Hansoo";
+    } else {
+        cout << res;
+    }
 }
